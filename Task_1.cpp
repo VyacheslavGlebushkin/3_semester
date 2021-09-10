438b2e5225362d6faa9fbbf3ef3794f7d3c87cfd
Vyacheslav Glebushkin <73432178+VyacheslavGlebushkin@users.noreply.github.com>
2021-09-10 23:50:09 +0300

    Add files via upload

---
+++ Task_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+//Номер варианта – остаток от деления суммы всех букв фамилии на число вариантов
+void Variant() {
+	string surname;
+	int n;
+	cout << "Введите фамилию студента: ";
+	cin >> surname;
+	cout << "Введите количество вариантов: ";
+	cin >> n;
+	if (surname.length() % n > 0)
+		cout << "Номер варианта для студента с фамилией " << surname << ": " << surname.length() % n << endl;
+	else //Если остаток от деления - 0, вариант - 1
+		cout << "Номер варианта для студента с фамилией " << surname << ": 1" << endl;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Variant();
+	return 0;
+}
